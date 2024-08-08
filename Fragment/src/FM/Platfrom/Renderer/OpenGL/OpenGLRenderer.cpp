76bde4273f7adec00b49b062ce640d02d5ad8960
HemoDev345 <hemogalaxy345@gmail.com>
2024-08-08 11:45:29 +0300

    Remove Log Messages

--- Fragment/src/FM/Platfrom/Renderer/OpenGL/OpenGLRenderer.cpp
+++ Fragment/src/FM/Platfrom/Renderer/OpenGL/OpenGLRenderer.cpp
@@ -1,7 +1,6 @@
 #if FM_RENDER_API_OPENGL
 
 #include "FM/Platfrom/Renderer/Renderer.hpp"
-#include "FM/Core/Log.hpp"
 
 #include <GL/glx.h>
 
@@ -19,10 +18,6 @@ namespace fm
 
     void Renderer::InitImpl(Window& window)
     {
-        FM_LOG_INFO("GL Vendor: {}", (char*)glGetString(GL_VENDOR));
-        FM_LOG_INFO("GL Renderer: {}", (char*)glGetString(GL_RENDERER));
-        FM_LOG_INFO("GL Version: {}", (char*)glGetString(GL_VERSION));
-        FM_LOG_INFO("GL Shading Language: {}", (char*)glGetString(GL_SHADING_LANGUAGE_VERSION));
         window.InitRenderContext();
     }
 
